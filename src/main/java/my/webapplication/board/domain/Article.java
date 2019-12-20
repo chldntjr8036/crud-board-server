142c4d70db04d886c29eb133328ea7e2b5654466
chldntjr8036 <cwooseok60@gmail.com>
2019-12-20 16:03:38 +0900

    Revert "issue 2 : Article 리팩토링(setter 메서드를 체인방식으로 구현)"

--- src/main/java/my/webapplication/board/domain/Article.java
+++ src/main/java/my/webapplication/board/domain/Article.java
@@ -4,12 +4,12 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.Arrays;
 
 @Entity
 @Data
 @NoArgsConstructor(force = true)
 @AllArgsConstructor
+@Setter
 public class Article {
 
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
@@ -20,34 +20,4 @@ public class Article {
     private String password, contents;
     @Column(name = "DATETIME") private LocalDateTime dateTime;
     @Column(name = "VIEW_COUNT") private int viewCount;
-
-    public Article setTitle(String title) {
-        this.title = title;
-        return this;
-    }
-
-    public Article setUserName(String userName) {
-        this.userName = userName;
-        return this;
-    }
-
-    public Article setPassword(String password) {
-        this.password = password;
-        return this;
-    }
-
-    public Article setContents(String contents) {
-        this.contents = contents;
-        return this;
-    }
-
-    public Article setDateTime(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
-        return this;
-    }
-
-    public Article setViewCount(int viewCount) {
-        this.viewCount = viewCount;
-        return this;
-    }
 }
